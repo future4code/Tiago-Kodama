b2a6bf31bbd7bc998d8cf53c717661d3e52c239c
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-06-25 15:27:20 -0300

    Projeto semana 2

--- semana2/blackjack/desafio.js
+++ semana2/blackjack/desafio.js
@@ -1,13 +1,86 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
 
+const desafio = {
+   ...nossoJogo,
+
+   intencaoJogar() {
+      const numeroRodadas = this.ganhadorRodada.length
+      let mensagem = ""
+
+      if( !numeroRodadas )
+         mensagem += 'Bem vindo ao jogo de Blackjack! \n \n'
+   
+      mensagem += numeroRodadas? "Quer jogar de novo?" : "Que tal uma jogada?"
+
+      return confirm(mensagem)
+   },
+
+   rodadaUsuario(){
+      this.usuario.addCarta()
+
+      if( this.usuario.pontuacao > 21 )
+         return false
+
+      const mensagem = `Suas cartas são: ${this.usuario.getCartas()}. 
+      A carta revelada do computador é: ${this.computador.getCartas()}. \n 
+      Deseja comprar mais uma carta?`
+
+      const maisCartas = confirm( mensagem )
+
+      if( maisCartas )
+         return true
+
+      if( this.usuario.pontuacao === 22 ){
+         console.log('Tirou duplo A. Vou dar outras cartas')
+         this.usuario.jogarForaCartas()
+         return true
+      }
+      
+      return false
+   },
+     
+   rodadaComputador() {
+
+      const pontosComputador = this.computador.pontuacao
+      const pontosUsuario = this.usuario.pontuacao
+
+      if( pontosUsuario > 21 )
+         return false
+
+      else if( pontosComputador < pontosUsuario ){
+         this.computador.addCarta()
+         return true
+      }
+
+      return false
+   },
+
+   fimJogo() {
+      const usuario = this.usuario
+      const computador = this.computador
+      const ganhadorRodada = this.ganhadorRodada
+
+      let mensagem = ""
+
+      mensagem += `Usuário - cartas ${usuario.getCartas()} - pontuação ${usuario.pontuacao} \n`
+      mensagem += `Computador - cartas ${computador.getCartas()} - pontuação ${computador.pontuacao} \n`
+
+      switch( ganhadorRodada[ganhadorRodada.length-1] ){
+         case "Empate":
+            mensagem += "Empate!"
+            break
+         case "Computador":
+            mensagem += "O computador ganhou!"
+            break
+         case "Usuário":
+            mensagem += "O usuário ganhou!"
+            break
+      }
+
+      alert(mensagem)
+   }
+}
+
+/**
+ * START
+//  */
+main(desafio)
--- semana2/blackjack/nossoJogo.js
+++ semana2/blackjack/nossoJogo.js
@@ -1,27 +1,130 @@
-console.log('Bem vindo ao jogo de Backacj!')
+function main ( jogo ){
 
-let numeroRodadas = 0
-
-while(numeroRodadas? confirm("Quer iniciar uma nova rodada?") : confirm("Vamos jogar?")){
-
-   const carta1Usuario = comprarCarta()
-   const carta2Usuario = comprarCarta()
-   const carta1Computador = comprarCarta()
-   const carta2Computador = comprarCarta()
-   const pontuacaoUsuario = carta1Usuario.valor+carta2Usuario.valor
-   const pontuacaoComputador = carta1Computador.valor+carta2Computador.valor
-   numeroRodadas ++
-   
-   console.log(`Jogo ${numeroRodadas}`)
-   console.log(`Usuário - cartas ${carta1Usuario.texto} ${carta2Usuario.texto} - pontuação ${pontuacaoUsuario}`)
-   console.log(`Computador - cartas ${carta1Computador.texto} ${pontuacaoComputador} - pontuação ${pontuacaoComputador}`)
-
-   if( pontuacaoComputador>21 && pontuacaoUsuario>21 || pontuacaoComputador === pontuacaoUsuario ) 
-      console.log("Empate!")
-   else if( pontuacaoComputador<=21 && pontuacaoUsuario>21 || pontuacaoComputador > pontuacaoUsuario ) 
-      console.log("O computador ganhou")
-   else 
-      console.log("O usuário ganhou")
-
-   console.log("O jogo acabou \n \n")
+   while( jogo.intencaoJogar() ){
+      jogo.iniciar()
+      console.log("Fim de rodada \n \n")
+   } 
+   console.log("Fim")
 }
+
+const nossoJogo = {
+   usuario: {},
+   computador: {},
+   ganhadorRodada: [],
+
+   criarJogador(){
+      return {
+         cartas: [],
+         pontuacao: 0,
+         addCarta (){
+            const carta = comprarCarta()
+            this.cartas.push(carta.texto)
+            this.pontuacao += carta.valor
+         },
+         getCartas (){
+            return this.cartas.join(" ")
+         },
+         jogarForaCartas (){
+            this.cartas = []
+            this.pontuacao = 0
+         }
+      }
+   },
+
+   intencaoJogar() {
+      const numeroRodadas = this.ganhadorRodada.length
+      let mensagem = ""
+
+      if( !numeroRodadas )
+         console.log('Bem vindo ao jogo de Blackjack! \n \n')
+   
+      mensagem += numeroRodadas? "Quer jogar de novo?" : "Que tal uma jogada?"
+
+      return confirm(mensagem)
+   },
+
+   iniciar() {
+      this.usuario = this.criarJogador()
+      this.computador = this.criarJogador()
+
+      // Computador começa com uma carta
+      this.computador.addCarta()
+
+      let usuarioJoga = this.rodadaUsuario()
+      while(usuarioJoga){
+         usuarioJoga = this.rodadaUsuario()
+      }
+
+      let computadorJoga = this.rodadaComputador()
+      while(computadorJoga){
+         computadorJoga = this.rodadaComputador()
+      }
+      this.contarPontos()
+      this.fimJogo()
+   },
+
+   rodadaUsuario(){
+      this.usuario.addCarta()
+      this.usuario.addCarta()
+
+      return false
+   },
+
+   rodadaComputador() {
+      this.computador.addCarta()
+
+      return false
+   },
+
+   contarPontos(){
+      const pontosComputador = this.computador.pontuacao
+      const pontosUsuario = this.usuario.pontuacao
+
+      if( pontosUsuario>21 && pontosComputador>21 ){
+         this.ganhadorRodada.push("Empate")
+      }
+      else if( pontosComputador>21 && pontosUsuario<=21 ){
+         this.ganhadorRodada.push("Usuário")
+
+      }
+      else if( pontosComputador<=21 && pontosUsuario>21 ){
+         this.ganhadorRodada.push("Computador")
+
+      }
+      else{
+         if( pontosUsuario>pontosComputador )
+            this.ganhadorRodada.push("Usuário")
+         else if( pontosUsuario<pontosComputador )
+            this.ganhadorRodada.push("Computador")
+         else
+            this.ganhadorRodada.push("Empate")
+      }
+   },
+
+   fimJogo() {
+      const usuario = this.usuario
+      const computador = this.computador
+      const ganhadorRodada = this.ganhadorRodada
+
+      console.log(`Usuário - cartas ${usuario.getCartas()} - pontuação ${usuario.pontuacao}`)
+      console.log(`Computador - cartas ${computador.getCartas()} - pontuação ${computador.pontuacao}`)
+
+      switch( ganhadorRodada[ganhadorRodada.length-1] ){
+         case "Empate":
+            console.log("Empate!")
+            break
+         case "Computador":
+            console.log("O computador ganhou!")
+            break
+         case "Usuário":
+            console.log("O usuário ganhou!")
+            break
+      }
+   }
+}
+
+/**
+ * START
+ */
+// main(nossoJogo)
+
