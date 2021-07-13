062985c098fd59a4c989046317c032b055498155
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-07-13 12:58:02 -0300

    CardPqueno: style-components

--- semana5/labedin/src/components/CardPequeno/CardPequeno.js
+++ semana5/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import './CardPequeno.css';
+// import './CardPequeno.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 100%;
+    height: 7rem;
+    padding: 5%;
+    border: 1px solid black;
+`;
+
+const Imagem = styled.img`
+    width: 30px;
+    margin-right: 10px;
+`;
 
 function CardPequeno(props) {
     return (
-        <div className="card-pequeno-container">
-            <img src={ props.imagem } alt={ props.title }/>
+        <Container>
+            <Imagem 
+                src={ props.imagem }
+                />
             <p><strong>{props.title}: </strong> { props.texto }</p>
-        </div>
-
+        </Container>
     )
 }
 
