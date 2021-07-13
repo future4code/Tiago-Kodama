ad41e360c129f05953576d3720a78ef84bd7f56f
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-07-13 13:08:21 -0300

    ImagemButton: styled-components

--- semana5/labedin/src/components/ImagemButton/ImagemButton.js
+++ semana5/labedin/src/components/ImagemButton/ImagemButton.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import './ImagemButton.css'
+// import './ImagemButton.css'
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    border-radius: 50px;
+    width: 200px;
+    padding: 15px 30px;
+    margin: 10px auto;
+`;
+
+const Imagem = styled.img`
+    width: 30px;
+    margin-right: 10px;
+`
 
 function ImagemButton(props) {
     return (
-        <div className="image-button-container">
-            <img src={ props.imagem }/>
+        <Container>
+            <Imagem src={ props.imagem }/>
             <p>{ props.texto }</p>
-        </div>
+        </Container>
 
     )
 }
