eea93130961a21dbaa72567f5c1b10d564a4492e
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-07-19 14:24:46 -0300

    Etapa2: fix index question

--- semana6/labenu-forms/src/Componets/Etapa2.js
+++ semana6/labenu-forms/src/Componets/Etapa2.js
@@ -7,10 +7,10 @@ class Etapa2 extends React.Component {
             <>
                 <h2>ETAPA 2 - INFORMAÇÕES DO ENSINO SUPERIOR</h2>
 
-                <label for='curso'>Qual é o curso?</label>
+                <label for='curso'>1- Qual é o curso?</label>
                 <input id='curso' name='curso' />
 
-                <label for='unidadeDeEnsino'>Qual é a unidade de ensino?</label>
+                <label for='unidadeDeEnsino'>2- Qual é a unidade de ensino?</label>
                 <input id='unidadeDeEnsino' name='unidadeDeEnsino' />
 
                 <button onClick={this.props.handleButton}>Próxima etapa</button>
