8d7a4c482aa88b60049f785216bdbb96609ebde1
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-07-27 12:20:12 -0300

    App: Created

--- semana7/labenusers/src/App.js
+++ semana7/labenusers/src/App.js
@@ -3,6 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header>
+        <h1>Labenusers</h1>
+        <nav>
+          <div>
+            <button>Sign up</button>
+            <button>Users</button>
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        <p>Sign up</p>
+        <p>Users </p>
+      </main>
 
     </div>
   );
