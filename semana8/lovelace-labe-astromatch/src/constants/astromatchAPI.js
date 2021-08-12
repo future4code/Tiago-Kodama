9d5916aae50907af4ca4352fd7b72e32af202e0a
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-08-12 12:23:35 -0300

    clean

--- semana8/lovelace-labe-astromatch/src/constants/astromatchAPI.js
+++ semana8/lovelace-labe-astromatch/src/constants/astromatchAPI.js
@@ -3,3 +3,4 @@ export const nome_usuario = 'tiago-harumitsu-kodama'
 export const getProfileURL = `https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${nome_usuario}/person`
 export const choosePersonURL = `https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${nome_usuario}/choose-person`
 export const getMatchesURL = `https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${nome_usuario}/matches`
+export const cleanURL = `https://us-central1-missao-newton.cloudfunctions.net/astroMatch/${nome_usuario}/clear`
--- semana8/lovelace-labe-astromatch/src/pages/Clean/index.js
+++ semana8/lovelace-labe-astromatch/src/pages/Clean/index.js
@@ -1,8 +1,43 @@
-import {StyledContainerClean} from './styled'
+import { StyledContainerClean } from './styled'
 
-export default function Clean(){
+import axios from 'axios'
+import { cleanURL } from '../../constants/astromatchAPI'
+import { useState } from 'react'
+
+export default function Clean() {
+
+    const [message, setMessage] = useState('')
+
+    const handleButtonCleanData = async () => {
+        try {
+            await axios.put(cleanURL)
+            setMessage('Dados limpados com sucesso.')
+
+        } catch (error) {
+            console.error(error.response)
+            setMessage('Falha ao tentar limpar.')
+        }
+    }
+
+    const render = () => {
+        if (!message) {
+            return (
+                <>
+                    <p>Tem certeza que deseja limpar os dados?</p>
+                    <button
+                        onClick={handleButtonCleanData}
+                    >Remover</button>
+                </>
+            );
+        }
+        else {
+            return (
+                <p>{message}</p>
+            );
+        }
+    }
+
     return <StyledContainerClean>
-        <p>Tem certeza que deseja limpar os dados?</p>
-        <button>Remover</button>
+        {render()}
     </StyledContainerClean>
 }
