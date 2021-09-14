0e614dfa475cdf735a46703c19d435b34967792e
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-09-14 12:22:00 -0300

    buttons controllers

--- semana14/personare/src/components/board/index.js
+++ semana14/personare/src/components/board/index.js
@@ -1,9 +1,11 @@
-import { StyledBoard } from './styled'
+import { StyledBoard, StyledButtonLeft, StyledButtonRight } from './styled'
 
 export default function Board({children}){
     return (
         <StyledBoard>
+            <StyledButtonLeft>Prev</StyledButtonLeft>
             {children}
+            <StyledButtonRight>Next</StyledButtonRight>
         </StyledBoard>
     );
 }
--- semana14/personare/src/components/board/styled.js
+++ semana14/personare/src/components/board/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { colorPrimary } from '../../constants/colors/theme'
 
 export const StyledBoard = styled.div`
     width: 100vw;
@@ -7,4 +8,29 @@ export const StyledBoard = styled.div`
     align-items: center;
     overflow-x: hidden;
     background: gray;
+    position: relative;
+`
+    
+export const StyledButtonLeft = styled.button`
+    position: absolute;
+    left: 10px;
+
+    border-radius: 5px;
+    padding: 5px;
+    border: none;
+    color: white;
+    background: ${colorPrimary};
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+
+`
+export const StyledButtonRight = styled.button`
+    position: absolute;
+    right: 10px;
+
+    border-radius: 5px;
+    padding: 5px;
+    border: none;
+    color: white;
+    background: ${colorPrimary};
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
 `
