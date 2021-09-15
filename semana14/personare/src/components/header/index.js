b5b1bc73b3800064eac461f1b26a1da17d02bc33
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-09-15 10:07:06 -0300

    title

--- semana14/personare/src/components/header/index.js
+++ semana14/personare/src/components/header/index.js
@@ -2,16 +2,27 @@ import { useHistory } from 'react-router';
 import { StyledHeader, StyledButton, StyledTitle } from './styled'
 import arrowButtonIcon from '../../assets/img/arrow.svg'
 
-export default function Header(){
+export default function Header() {
     const history = useHistory()
+
+    const title = () => {
+        switch (history.location.pathname) {
+            case '/':
+                return 'Tarot'
+            case '/options':
+                return 'Options'
+            default:
+                return 'Detail';
+        }
+    }
 
     return (
         <StyledHeader>
-            {history.location.pathname!=='/' &&
-            <StyledButton
-                onClick={() => history.push('/')}
-            ><img src={arrowButtonIcon} alt='button icon'/></StyledButton>}
-            <StyledTitle>Title</StyledTitle>
+            {history.location.pathname !== '/' &&
+                <StyledButton
+                    onClick={() => history.push('/')}
+                ><img src={arrowButtonIcon} alt='button icon' /></StyledButton>}
+            <StyledTitle>{title()}</StyledTitle>
         </StyledHeader>
     );
 }
