93fd49f0d05b2bae133aebfeb7b9b875a509b25b
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-09-20 13:55:31 -0300

    handle errors task2

--- semana15/nodeEPackage/task2/index.js
+++ semana15/nodeEPackage/task2/index.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk')
+
 const calculator = (
     opation, 
     numberValue1, 
@@ -21,6 +23,21 @@ const calculator = (
     }
 } 
 
-const [,,op, v1, v2] = process.argv
 
-console.log(calculator(ob, v1, v2))
+
+const main = () => {
+    try {
+        const [,,op, v1, v2] = process.argv
+
+        if(!op||!v1||!v2) 
+            throw new Error('Esperava três argumentos.')
+        
+        console.log(calculator(op, v1, v2))
+       
+    } catch (error) {
+        console.error(chalk.red(`Erro: ${error.message}`),)
+    }
+}
+
+
+main()
