007b0ec5b2f930a12e022292ba78f9120a9da862
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-09-20 13:23:25 -0300

    task2

--- semana15/nodeEPackage/index.js
+++ semana15/nodeEPackage/index.js
@@ -1,2 +1,7 @@
 console.log('Start application')
 
+const {calculator} = require('./src/exercicio2')
+
+const [,,operation, value1, value2] = process.argv
+
+console.log(calculator(operation, Number(value1), Number(value2)))
