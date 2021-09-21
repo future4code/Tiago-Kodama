9cc819176b7e2a677ce2b3eefb18275d6f00bb4b
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-09-21 12:52:24 -0300

    atividade 4

---
+++ semana15/introducaoTypescript/src/exercicio4.ts
@@ -0,0 +1,40 @@
+/**
+ * tsc exercicio4.ts
+ */
+
+type pokemon = {
+  name: string;
+  types: string;
+  healthPoints: number;
+};
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28,
+};
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31,
+};
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35,
+};
+
+/**
+ * tsc src/exercicio4.ts
+ */
+
+/**
+ * criando uma pasta tsconfig e tsc src
+ */
+
+/**
+ * mais opções surgiram
+ */
+
