d1e860db11dbfd28edde9194c245afcf64edcb70
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-09-24 11:16:28 -0300

    definir types

---
+++ semana15/labebank/src/constants/types.ts
@@ -0,0 +1,12 @@
+type movimento = {
+    cpf: string,
+    valor: number,
+    data: Date,
+    descricao?: string
+}
+
+type Usuario = {
+    nome: string,
+    cpf: string,
+    dataNascimento: Date,
+}
