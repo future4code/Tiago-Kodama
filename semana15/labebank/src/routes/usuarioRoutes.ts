56c12ce5ea5a4d81ad8627949edae8821d8e0093
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-09-25 19:54:23 -0300

    ponto e virgulas

--- semana15/labebank/src/routes/usuarioRoutes.ts
+++ semana15/labebank/src/routes/usuarioRoutes.ts
@@ -7,12 +7,12 @@ import {
     transferenciaInterna
 } from "../controllers/usuarioController"
 
-const router = Router();
+const router = Router()
 
 router.get('/usuario', pegarUsuarios)
-router.post('/usuario/criar-conta', criarConta);
-router.post('/usuario/adicionar-saldo', adicionarSaldo);
-router.post('/usuario/consultar-saldo', consultarSaldo);
+router.post('/usuario/criar-conta', criarConta)
+router.post('/usuario/adicionar-saldo', adicionarSaldo)
+router.post('/usuario/consultar-saldo', consultarSaldo)
 router.post('/usuario/transferencia-interna', transferenciaInterna)
 
 export default router;
