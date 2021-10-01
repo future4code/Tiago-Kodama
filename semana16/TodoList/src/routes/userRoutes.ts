7cf93817d31c3f60ed4042e837c04794bdc6d7bc
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-10-01 12:50:06 -0300

    user routes

--- semana16/TodoList/src/routes/userRoutes.ts
+++ semana16/TodoList/src/routes/userRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import {
+    createUserController,
+    getUserByIdController,
+    updateUserController
+} from "../controllers/userController";
 
 const router = Router()
 
-router.post('/user')
-router.put('/user/edit/:id')
-router.get('/user/:id')
+router.post('/user', createUserController)
+router.put('/user/edit/:id', updateUserController)
+router.get('/user/:id', getUserByIdController)
 
 export default router
