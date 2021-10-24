ab336c7ea3c360615d8703797f678c549f6c1d04
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-10-24 20:17:52 -0300

    login route

--- semana19/src/routes/userRoutes.ts
+++ semana19/src/routes/userRoutes.ts
@@ -1,14 +1,18 @@
 import { Request, Response, Router } from "express";
 import { createUserController } from "../useCase/createUser";
 import { getUserIntoController } from "../useCase/getUserInfo";
+import { loginUserController } from "../useCase/loginUser";
 
-const router = Router()
+const router = Router();
 
-router.post('/signup', async (req: Request, res: Response) => {
-    await createUserController.handle(req, res)
-})
-router.get('/user/:id', async (req: Request, res: Response) => {
-    await getUserIntoController.handle(req, res)
-})
+router.post("/login", async (req: Request, res: Response) => {
+  await loginUserController.handle(req, res);
+});
+router.post("/signup", async (req: Request, res: Response) => {
+  await createUserController.handle(req, res);
+});
+router.get("/user/:id", async (req: Request, res: Response) => {
+  await getUserIntoController.handle(req, res);
+});
 
-export default router
+export default router;
