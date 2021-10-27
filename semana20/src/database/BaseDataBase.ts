d21118de3ca1ad9bfe5015e84aa3f4564b870095
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-10-26 22:07:19 -0300

    base data base

---
+++ semana20/src/database/BaseDataBase.ts
@@ -0,0 +1,19 @@
+import knex, { Knex } from "knex";
+
+export class BaseDataBase {
+    protected static connection: Knex = knex({
+        client: "mysql",
+        connection: {
+           host: process.env.DB_HOST,
+           user: process.env.DB_USER,
+           password: process.env.DB_PASSWORD,
+           database: process.env.DB_SCHEMA,
+           port: 3306,
+           multipleStatements: true
+        }
+     })
+
+     close(){
+         BaseDataBase.connection.destroy()
+     }
+}
