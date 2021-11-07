cdfc451b00a6761399bf09c99b4891f20f8bbee8
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-11-07 14:17:04 -0300

    params

--- semana21/src/routes/index.ts
+++ semana21/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import getPokemonByIdRouter from "./getPokemonsByIdRoute"
 import getPokemonsByPageRouter from "./getPokemonsByPageRoute"
+import getPokemonsbyParams from "./getPokemonsByParamsRoute"
 
 const router = Router()
 
 router.use("/", getPokemonsByPageRouter)
 router.use("/", getPokemonByIdRouter)
+router.use("/", getPokemonsbyParams)
 
 export { router }
 
